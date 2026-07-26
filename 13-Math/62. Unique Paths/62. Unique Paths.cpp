3b4950fbad84b611b6caebe59bd157157d20b8c0
Ayush Kumar <ayushbhaiya100@gmail.com>
2026-07-27 00:32:30 +0530

    [62] [Time Beats: 100%] [Memory Beats: 21.02%] - Leet2Hub

--- 13-Math/62. Unique Paths/62. Unique Paths.cpp
+++ 13-Math/62. Unique Paths/62. Unique Paths.cpp
@@ -1,17 +1,16 @@
-1class Solution {
-2public:
-3    int uniquePaths(int m, int n) {
-4        int sum=m+n-2;
-5        int maxi=max(m-1,n-1);
-6        double upper=1,lower=1;
-7
-8        for(int i=sum;i>maxi;i--){
-9            upper*=i;
-10        }
-11        int diff=sum-maxi;
-12        for(int i=diff;i>1;i--){
-13            lower*=i;
-14        }
-15        return upper/lower;
-16    }
-17};
+class Solution {
+public:
+    int solve(int i,int j,vector<vector<int>> &dp)
+    {
+        if(i==0 && j==0)    return 1;
+        if(i<0 || j<0)  return 0;
+        if(dp[i][j]!=-1)    return dp[i][j];
+        int up=solve(i-1,j,dp);
+        int left=solve(i,j-1,dp);
+        return dp[i][j]=up+left;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,dp);
+    }
+};
